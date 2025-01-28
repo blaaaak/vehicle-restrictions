4f3880495e1f6961db6c3721a1ff9f6ed736750b
blaaaak <103514165+blaaaak@users.noreply.github.com>
2025-01-28 23:52:50 +1100

    version 1.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ use_experimental_fxv2_oal 'yes'
 
 name 'Vehicle Restrictions'
 description 'Simple vehicle restrictions for FiveM'
-version '1.0.0'
+version '1.1.0'
 
 shared_script { '@ox_lib/init.lua' }
 
